7a9440b534d3bdced3892e72aabaf17289be27db
ShrikrishnaR <128310270+ShrikrishnaR-2004@users.noreply.github.com>
2025-02-12 10:22:31 +0530

    Time: 8284 ms (5.04%), Space: 17.8 MB (58.89%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.py
+++ 0507-perfect-number/0507-perfect-number.py
@@ -1,13 +1,22 @@
 class Solution:
     def checkPerfectNumber(self, num: int) -> bool:
-        if num <= 1:
+        # if num <= 1:
+        #     return False
+        
+        # divisor_sum = 1
+        # for i in range(2, int(num**0.5) + 1):
+        #     if num % i == 0:
+        #         divisor_sum += i
+        #         if i != num // i:
+        #             divisor_sum += num // i
+        
+        # return divisor_sum == num
+
+        if num%2==1:
             return False
         
-        divisor_sum = 1
-        for i in range(2, int(num**0.5) + 1):
-            if num % i == 0:
-                divisor_sum += i
-                if i != num // i:
-                    divisor_sum += num // i
-        
-        return divisor_sum == num
+        sum1=0
+        for i in range(1,num//2+1):
+            if num%i==0:
+                sum1+=i
+        return sum1==num
