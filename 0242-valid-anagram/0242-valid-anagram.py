514da2ea69452372014d0d91eb4cbce742c0f33e
ShrikrishnaR <128310270+ShrikrishnaR-2004@users.noreply.github.com>
2025-03-28 08:32:31 +0530

    Time: 15 ms (43.52%), Space: 18.6 MB (12.05%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s=s.lower()
+        t=t.lower()
+        if sorted(s)==sorted(t):
+            return True
+        else:
+            return False
